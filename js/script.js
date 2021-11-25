995354bb62e8735fcc95ef4d7536260c4d81a342
jwyc <janis.wy.chan@gmail.com>
2021-11-24 22:27:17 -0800

    added button eventListener, comment out notes

--- js/script.js
+++ js/script.js
@@ -6,32 +6,33 @@ const img = document.querySelector(".img");
 const getImage= async function(){
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
-    console.log(images);
+    //console.log(images);
     selectRandomImage(images);
-    
 }
-
 
 const selectRandomImage= function(images){
     const randomIndex = Math.floor(Math.random()*images.length);
     //console.log(randomIndex);
     // Error here fixed...once variable images is declared above you can now treat it as normal javascript and use index number as per usual. 
     const randomImage = images[randomIndex];
-    console.log(randomImage);
+    //console.log(randomImage);
     displayImage(randomImage);
 };
 
 const displayImage = function(randomImage){
     const author= randomImage.author;
     // because images variable declared you can use .notation as per usual to handle parameters.
-    console.log(author);
+    //console.log(author);
     const imageAddress = randomImage.download_url;
-    console.log(imageAddress);
+    //console.log(imageAddress);
     authorSpan.innerText = author;
     img.src = imageAddress;
     imgDiv.classList.remove("hide");
 };
 
-getImage();
 
+  
 
+button.addEventListener("click", function(){
+    getImage();
+});
